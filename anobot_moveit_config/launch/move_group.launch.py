69500f98ffb267dd997499d52c1d077611d4b049
khkoh <khkoh23@gmail.com>
2026-06-25 11:56:59 +0800

    Revamped the robot model: static objects are excluded.

--- anobot_moveit_config/launch/move_group.launch.py
+++ anobot_moveit_config/launch/move_group.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_move_group_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("anobot_cell", package_name="anobot_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("anobot", package_name="anobot_moveit_config").to_moveit_configs()
     return generate_move_group_launch(moveit_config)
